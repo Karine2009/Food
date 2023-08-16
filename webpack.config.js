120fee7318cafedbec8b78289da2ec1bb2eb1188
Karine Ghapoyan <karinegapoya44@gmail.com>
2023-08-16 11:25:32 +0300

    мы добавили веб-пакет в наш проект, затем мы настроили веб-пакет с конфигурационным файлом, затем мы сделали наш файл index.js типом компонентов, отредактировали файл packacge.json и добавили скрипт веб-пакета.

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+
+const path = require('path');
+
+module.exports = {
+	mode: "development", // production || development
+	entry: './src/js/index.js',
+	output: {
+		filename: 'all.js',
+		path: path.resolve(__dirname, 'dist/js/'),
+	},
+	watch: true,
+};
